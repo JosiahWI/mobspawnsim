af401f49b6dec63206427cdb5f5a39ba9b03db5f
JosiahWI <41302989+JosiahWI@users.noreply.github.com>
2018-07-28 18:20:05 -0500

    add some explanation to mobSpawn.py

--- spawnData.py
+++ spawnData.py
@@ -1,3 +1,12 @@
+#This is the definition of effects.
+#There are three types of powers an ore can have:
+#1: overrides.
+#Overrides are only for the most powerful ores, these rewrite an effect of another ore of any prioritythey can't override overrides or additions.)
+#2: additions.
+#Additions are for moderate and high powered ores, they extend the effect of another ore by adding an extra calculation to an existing effect.
+#3: effects.
+#Effects are the most basic power. An effect takes a base chance that a mob will spawn, and applies an equation to it to make a new chance.
+
 ores = {
     "light" :
         {
